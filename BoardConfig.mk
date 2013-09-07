8502bfe1e7a6dea36d0bb6de7d1efda78b7beb2a
Christopher R. Palmer <crpalmer@gmail.com>
2013-09-07 06:59:21 -0400

    kernel: add some extra space before the ramdisk
    If you compile with -O3 (particularly with Linaro 4.8) then the
    kernel starts to get larger than the allocated space and the
    device will not boot.
    
    Change-Id: I5d24f75dd72e0778620d8b8f97cff6e363e47181

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlx user_debug=0
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01a00000
 TARGET_KERNEL_CONFIG := cyanogenmod_dlx_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/m7
 
